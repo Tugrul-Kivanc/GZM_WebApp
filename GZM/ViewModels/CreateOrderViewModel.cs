63162e9ec0d80b8d6f816ba3949f9d8fc0935d98
TK <tgrl@protonmail.com>
2023-08-15 21:44:38 +0300

    Added CreateOrderViewModel

---
+++ GZM/ViewModels/CreateOrderViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DatabaseModel.Models;
+
+namespace GZM.ViewModels
+{
+    public class CreateOrderViewModel
+    {
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+        public List<Product> Products { get; set; }
+        public int Quantity { get; set; }
+        public int Fee { get; set; }
+        public string Payment { get; set; } = null!;
+        public string? Description { get; set; }
+    }
+}
