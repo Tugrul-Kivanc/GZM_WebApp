047024fd3e7cd46f33ae70127e96c125f41658cd
TK <tgrl@protonmail.com>
2023-08-23 12:13:06 +0300

    Removed ProductCount

--- GZM/ViewModels/CreateOrderViewModel.cs
+++ GZM/ViewModels/CreateOrderViewModel.cs
@@ -4,7 +4,6 @@
     {
         public DateTime OrderDate { get; set; } = DateTime.Now;
         public List<int> ProductIds { get; set; }
-        public int ProductCount { get; set; }
         public int Fee { get; set; }
         public string Payment { get; set; } = null!;
         public string? Description { get; set; }
